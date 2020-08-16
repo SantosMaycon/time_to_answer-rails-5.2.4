e8ab1131097974d9f30e6e3b41101c096b7b7225
SantosMaycon <mayconalves.santos14@gmail.com>
2020-08-16 18:45:00 +0000

    using scope for query

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,13 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+
+  scope :_search_, ->(term, page) { 
+    Question.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page).per(7) 
+  }
+
+  scope :last_questions, ->(page) { 
+    Question.includes(:answers).order('created_at desc').page(page).per(3) 
+  }  
 end
