00e3afdef6d874b428c2aa4a1499f67c55076d37
SantosMaycon <mayconalves.santos14@gmail.com>
2020-08-19 00:28:11 +0000

    removing the external admin registration

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :questions # questions crud
   end
   devise_for :users
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'site/welcome#index'
